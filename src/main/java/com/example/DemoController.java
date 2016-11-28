3e921e24d6e8138bb7d94e7dd3eb6fb933bed802
pasco3596 <halhogechi@gmail.com>
2016-11-29 04:26:34 +0900

    demo3

--- src/main/java/com/example/DemoController.java
+++ src/main/java/com/example/DemoController.java
@@ -5,15 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
-import java.lang.reflect.Method;
 
 /**
  * Created by pasuco on 2016/11/29.
  */
 @Controller
-@RequestMapping(value ="/")
+@RequestMapping(value ="/demo")
 public class DemoController {
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model){
         return "index";
     }

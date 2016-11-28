c6c0c53e210e33f44b89094c83c30794172136e0
pasco3596 <halhogechi@gmail.com>
2016-11-29 06:27:49 +0900

    demo3

--- src/main/java/com/example/DemoController.java
+++ src/main/java/com/example/DemoController.java
@@ -1,5 +1,6 @@
 package com.example;
 
+import org.springframework.boot.Banner;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,19 @@ import org.springframework.web.bind.annotation.RequestMethod;
  * Created by pasuco on 2016/11/29.
  */
 @Controller
-@RequestMapping(value ="/demo")
+@RequestMapping(value ="/")
 public class DemoController {
     @RequestMapping(value = "", method = RequestMethod.GET)
     public String index(Model model){
         return "index";
+    }
+    @RequestMapping(value = "/demo", method = RequestMethod.GET)
+    public String demo(Model model){
+        return "demo";
+    }
+
+    @RequestMapping(value = "/test", method = RequestMethod.GET)
+    public String test(Model model){
+        return "test";
     }
 }
